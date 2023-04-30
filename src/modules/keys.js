b4429c66aebd249cab68a97c8549f95c1da95517
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-05-01 01:44:09 +0200

    feat: add obj with data of few btns to write and test functions

---
+++ src/modules/keys.js
@@ -0,0 +1,50 @@
+const keys = {
+  en: {
+    'Backquote': {
+      'key': '§',
+      'altKey': false,
+      'ctrlKey': false,
+      'metaKey': false,
+      'shiftKey': false,
+    },
+    'Digit1': {
+      'key': '1',
+      'altKey': false,
+      'ctrlKey': false,
+      'metaKey': false,
+      'shiftKey': false,
+    },
+    'KeyQ': {
+      'key': 'q',
+      'altKey': false,
+      'ctrlKey': false,
+      'metaKey': false,
+      'shiftKey': false,
+    },
+    'ShiftLeft': {
+      'key': 'Shift',
+      'altKey': false,
+      'ctrlKey': false,
+      'metaKey': false,
+      'shiftKey': false,
+    },
+    'Space': {
+      'key': ' ',
+      'altKey': false,
+      'ctrlKey': false,
+      'metaKey': false,
+      'shiftKey': false,
+    },
+    'Tab': {
+      'key': 'Tab',
+      'altKey': false,
+      'ctrlKey': false,
+      'metaKey': false,
+      'shiftKey': false,
+    },
+
+  },
+  ru: {}
+}
+
+export default keys;
