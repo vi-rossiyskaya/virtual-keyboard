60b1fa217875c867c4ed644b6b3e3add6cbd4bb6
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-04-30 16:24:28 +0200

    feat: make basic structure for keyboard creation and testing

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const body = document.querySelector('body');
+const main = document.createElement('main');
+const textArea = document.createElement('textarea');
+const keyboardContainer = document.createElement('div');
+
+main.classList.add('main');
+textArea.classList.add('textarea');
+keyboardContainer.classList.add('keyboard-container');
+
+main.append(textArea, keyboardContainer);
+body.append(main);
