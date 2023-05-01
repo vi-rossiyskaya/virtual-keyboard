bbac77b853cd28ccf884196a820885b09f48c0cd
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-05-01 13:31:53 +0200

    feat: tab only insert value

--- src/modules/keyboard-listener.js
+++ src/modules/keyboard-listener.js
@@ -1,4 +1,8 @@
 export default function keyboardListener(event) {
+  console.log(event)
+  if (event.code === 'Tab') {
+    event.preventDefault();
+  }
   const targetBtn = document.querySelector(`[data-code=${event.code}]`);
   if (targetBtn) {
     const click = new Event('click');
