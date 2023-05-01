528b3b144704d68c6793cef935853059c67a33e2
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-05-02 00:06:15 +0200

    feat: add message about OS and key sequence for lang switch

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ import handleKeyUp from './modules/keyUp-listener';
 
 const body = document.querySelector('body');
 const main = document.createElement('main');
+const paragraph = document.createElement('p');
 const textArea = document.createElement('textarea');
 const keyboardContainer = document.createElement('div');
 
 main.classList.add('main');
+paragraph.classList.add('message');
 textArea.classList.add('textarea');
 keyboardContainer.classList.add('keyboard-container');
+paragraph.textContent = `Клавиатура создана в macOS. \n Для переключения используйте Ctrl + Alt`
 
-main.append(textArea, keyboardContainer);
+main.append(paragraph ,textArea, keyboardContainer);
 body.append(main);
 
 renderButtons('en', keyboardContainer, keys);
