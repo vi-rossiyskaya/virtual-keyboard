5f62d326d035a2f3fa5f708910fabefa71c539ea
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-05-01 13:21:14 +0200

    feat: prevent mistake with unexisting key for this keyboard

--- src/modules/keyboard-listener.js
+++ src/modules/keyboard-listener.js
@@ -1,5 +1,7 @@
 export default function keyboardListener(event) {
   const targetBtn = document.querySelector(`[data-code=${event.code}]`);
-  const click = new Event('click');
-  targetBtn.dispatchEvent(click);
+  if (targetBtn) {
+    const click = new Event('click');
+    targetBtn.dispatchEvent(click);
+  }
 }
