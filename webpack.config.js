ca8f3f5eb0c74fc9ec0f133b8b0d856cf1da14a7
vi-rossiyskaya <v.i.rossiyskaya@gmail.com>
2023-06-22 16:13:53 +0200

    build: add font handling to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,11 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
